bdbd9a5566e215b981b659989848c651d84d78f7
Jose Luis <lozanodiaz.joseluis@live.com>
2017-03-22 16:52:38 +0100

    App : estructura principal de la APP

---
+++ CalculatorService.Client/CalculatorService.cs
@@ -0,0 +1,55 @@
+﻿using CalculatorService.Client.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService.Client
+{
+    public static class CalculatorService
+    {
+        private static string ENDPOINT = "http://localhost:17480/api/calculator/";
+
+        public static string testAdd()
+        {
+            // Request Test Object
+            AddRequest request = new AddRequest();
+            request.Addends = new int[] { 1, 2, 3 };
+
+            // Response Test Object
+            AddResponse response;
+            
+            // Calling service...
+            HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(String.Format("{0}{1}", ENDPOINT, "add"));
+            Req.Method = "POST";
+            Req.ContentType = "application/json";
+            
+            Req.Headers.Add("X-Evi-Tracking-Id", "12345678");
+
+            using (var streamWriter = new StreamWriter(Req.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(request);
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            // Getting response...
+            HttpWebResponse Resp = (HttpWebResponse)Req.GetResponse();
+
+            StreamReader sr = new StreamReader(Resp.GetResponseStream(), Encoding.UTF8);
+
+            string s = sr.ReadToEnd();
+            sr.Close();
+            Resp.Close();
+
+            response = JsonConvert.DeserializeObject<AddResponse>(s);
+            return response.Sum.ToString();            
+        }
+    }
+}
---
+++ CalculatorService.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, introduce a command: add | sub | mult | div | sqrt | query");
+            Console.Write(">>");
+
+            string command = Console.ReadLine();
+
+            while (!command.ToLower().Equals("exit"))
+            {
+                switch (command)
+                {
+                    case "add":
+                        Console.WriteLine(CalculatorService.testAdd());                        
+                        break;
+                    case "sub": break;
+                    case "mult": break;
+                    case "div": break;
+                    case "sqrt": break;
+                    case "query": break;
+                    default:
+                        Console.WriteLine("Invalid command. Try: add | sub | mult | div | sqrt | query");
+                        break;
+                }
+
+                Console.Write(">>");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
